1e645520c10ef7f53e3459cc469923033664ef19
lolhi <lolhi@naver.com>
2019-08-07 23:48:58 +0900

    router.js: add cancel clipping

--- router/router.js
+++ router/router.js
@@ -600,12 +600,17 @@ module.exports = function(app, request, config, RecipeBasics, RecipeMaterial, Re
             var flag = 0;
             for(k = 0; k < ud.CLIPPING.length; k++){
                 if(ud.CLIPPING[k].RECIPE_ID == req.body.RECIPE_ID){
+		    ud.CLIPPING.splice(k,1);
                     flag = 1;
                     break;
                 }
             }
             if(flag == 1){
-                res.status(200).end("exist");
+		UserData.DBname.findOneAndUpdate({ID: req.body.ID}, {$set:{CLIPPING: ud.CLIPPING}}, function(err1, reply){
+                    if(err1) return res.status(500).json({ error: "add clipping fail" });
+
+                    res.status(200).end('exist');
+                });
                 return;
             }
             var i;
